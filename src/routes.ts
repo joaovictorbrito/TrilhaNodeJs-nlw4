0d3f0180dadeb5cbc4b486a847ac295f945924b1
João Victor <joaobritosantos0@outlook.com>
2021-02-28 20:39:27 -0300

    Finalizando a api com validações

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,20 @@ import { Router } from 'express';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
+
 const surveysController = new SurveysController();
 
 const sendMailController = new SendMailController();
 
+const answerController = new AnswerController();
+
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 
@@ -18,5 +24,8 @@ router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
 
+router.get("/answers/:value", answerController.execute)
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
